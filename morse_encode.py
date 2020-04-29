aa0472b80860d5e4465d395c3ef03a8993f11368
jinnosux <vahid.k@ibu.edu.mk>
2020-04-29 19:48:21 +0200

    v2.1
    Added sounds as output

--- morse_encode.py
+++ morse_encode.py
@@ -1,3 +1,6 @@
+import sys
+import pygame
+import time
 import speech_recognition as sr
 r = sr.Recognizer()
 m = sr.Microphone()
@@ -32,9 +35,30 @@ def encode(message):
 
     # We need to join together Morse code letters with spaces
     morse_message = " ".join(morse)
-    
     return morse_message
 
-
 encoded_message = encode(message)
-print(f"Google recognized you said: {message}\nMorse encoded: {encoded_message}")
+
+s = input("For audio, press 1.\nFor a text, press 2:\n")
+if s == "1":
+    ONE_UNIT = 0.5
+    THREE_UNITS = 3 * ONE_UNIT
+    SEVEN_UNITS = 7 * ONE_UNIT
+    PATH = 'morse_sound_files/'
+
+    pygame.init()
+
+    for char in message:
+	    if char == ' ':
+		    print (' '*7 )
+		    time.sleep(SEVEN_UNITS)
+	    else:
+		    pygame.mixer.music.load(PATH + char.upper() + '_morse_code.ogg')
+		    pygame.mixer.music.play()
+		    time.sleep(THREE_UNITS)
+    print(f"Google recognized you said: {message}\nMorse encoded: {encoded_message}")
+elif s == "2" :
+    
+    print(f"Google recognized you said: {message}\nMorse encoded: {encoded_message}")
+else :
+    print("wrong option")
